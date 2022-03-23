d7a0dfe30be963473c990234fd9e25557df25bce
jeongogo <chcgogo2@naver.com>
2022-03-23 15:33:56 +0900

    chart

--- src/components/measure/PeriodicView.js
+++ src/components/measure/PeriodicView.js
@@ -11,7 +11,6 @@ import {
 } from "recharts";
 
 const PeriodicView = ({ periodic }) => {
-  const [fms, setFms] = useState([]);
   const [y_balance, setY_balance] = useState([]);
   const [tuck_jump, setTuck_jump] = useState([]);
   const [isometric_test, setIsometric_test] = useState([]);
@@ -27,190 +26,219 @@ const PeriodicView = ({ periodic }) => {
   const [push_up, setPush_up] = useState([]);
   const [medicine_ball_throwing, setMedicine_ball_throwing] = useState([]);
 
+  const fms = periodic.map((item) => {
+    return {
+      date: item.date,
+      overhead_deep_squat: item.injury_prevention.fms.overhead_deep_squat,
+      hurdle_step_left: item.injury_prevention.fms.hurdle_step_left,
+      hurdle_step_right: item.injury_prevention.fms.hurdle_step_right,
+      hurdle_step_total: item.injury_prevention.fms.hurdle_step_total,
+      inline_lunge_left: item.injury_prevention.fms.inline_lunge_left,
+      inline_lunge_right: item.injury_prevention.fms.inline_lunge_right,
+      inline_lunge_total: item.injury_prevention.fms.inline_lunge_total,
+      shoulder_mobility_left: item.injury_prevention.fms.shoulder_mobility_left,
+      shoulder_mobility_right: item.injury_prevention.fms.shoulder_mobility_right,
+      shoulder_mobility_total: item.injury_prevention.fms.shoulder_mobility_total,
+      active_straight_leg_raise_left:
+        item.injury_prevention.fms.active_straight_leg_raise_left,
+      active_straight_leg_raise_right:
+        item.injury_prevention.fms.active_straight_leg_raise_right,
+      active_straight_leg_raise_total:
+        item.injury_prevention.fms.active_straight_leg_raise_total,
+      trunk_stability_push_up: item.injury_prevention.fms.trunk_stability_push_up,
+      rotary_core_stability_left: item.injury_prevention.fms.rotary_core_stability_left,
+      rotary_core_stability_right: item.injury_prevention.fms.rotary_core_stability_right,
+      rotary_core_stability_total: item.injury_prevention.fms.rotary_core_stability_total,
+      total_score: item.injury_prevention.fms.total_score
+    }
+  });
+
   useEffect(() => {
-    if (periodic.length > 1) {
-      periodic.forEach((item) => {
-        setFms([
-          ...fms,
-          {
-            date: item.date,
-            overhead_deep_squat: item.fms.overhead_deep_squat,
-            hurdle_step_left: item.fms.hurdle_step_left,
-            hurdle_step_right: item.fms.hurdle_step_right,
-            hurdle_step_total: item.fms.hurdle_step_total,
-            inline_lunge_left: item.fms.inline_lunge_left,
-            inline_lunge_right: item.fms.inline_lunge_right,
-            inline_lunge_total: item.fms.inline_lunge_total,
-            shoulder_mobility_left: item.fms.shoulder_mobility_left,
-            shoulder_mobility_right: item.fms.shoulder_mobility_right,
-            shoulder_mobility_total: item.fms.shoulder_mobility_total,
-            active_straight_leg_raise_left:
-              item.fms.active_straight_leg_raise_left,
-            active_straight_leg_raise_right:
-              item.fms.active_straight_leg_raise_right,
-            active_straight_leg_raise_total:
-              item.fms.active_straight_leg_raise_total,
-            trunk_stability_push_up: item.fms.trunk_stability_push_up,
-            rotary_core_stability_left: item.fms.rotary_core_stability_left,
-            rotary_core_stability_right: item.fms.rotary_core_stability_right,
-            rotary_core_stability_total: item.fms.rotary_core_stability_total,
-            total_score: item.fms.total_score,
-          },
-        ]);
-        setY_balance([
-          ...y_balance,
-          {
-            date: item.date,
-            limb_length: item.y_balance.limb_length,
-            anterior_left: item.y_balance.anterior_left,
-            anterior_right: item.y_balance.anterior_right,
-            anterior_difference: item.y_balance.anterior_difference,
-            postero_lateral_left: item.y_balance.postero_lateral_left,
-            postero_lateral_right: item.y_balance.postero_lateral_right,
-            postero_lateral_difference:
-              item.y_balance.postero_lateral_difference,
-            postero_medial_left: item.y_balance.postero_medial_left,
-            postero_medial_right: item.y_balance.postero_medial_right,
-            postero_medial_difference: item.y_balance.postero_medial_difference,
-            composite_score_left: item.y_balance.composite_score_left,
-            composite_score_right: item.y_balance.composite_score_right,
-          },
-        ]);
-        setTuck_jump([
-          ...tuck_jump,
-          {
-            date: item.date,
-            score: item.tuck_jump.score,
-          },
-        ]);
-        setIsometric_test([
-          ...isometric_test,
-          {
-            date: item.date,
-            rail_point: item.isometric_test.rail_point,
-            max_force_left: item.isometric_test.max_force_left,
-            max_force_right: item.isometric_test.max_force_right,
-            max_force_difference: item.isometric_test.max_force_difference,
-            rfd_left: item.isometric_test.rfd_left,
-            rfd_right: item.isometric_test.rfd_right,
-            rfd_difference: item.isometric_test.rfd_difference,
-          },
-        ]);
-        setThree_pq_both_legs([
-          ...three_pq_both_legs,
-          {
-            date: item.date,
-            max_force: item.three_pq_both_legs.max_force,
-            max_power: item.three_pq_both_legs.max_power,
-            max_power_negative: item.three_pq_both_legs.max_power_negative,
-            average_peak_power: item.three_pq_both_legs.average_peak_power,
-            average_peak_power_negative:
-              item.three_pq_both_legs.average_peak_power_negative,
-            decrement: item.three_pq_both_legs.decrement,
-            decrement_negative: item.three_pq_both_legs.decrement_negative,
-          },
-        ]);
-        setStiffness_jump([
-          ...stiffness_jump,
-          {
-            date: item.date,
-            jump_height_max: item.stiffness_jump.jump_height_max,
-            jump_height_average: item.stiffness_jump.jump_height_average,
-            rsi_max: item.stiffness_jump.rsi_max,
-            rsi_average: item.stiffness_jump.rsi_average,
-          },
-        ]);
-        setStanding_reach([
-          ...standing_reach,
-          {
-            date: item.date,
-            base: item.standing_reach.base,
-            touch: item.standing_reach.touch,
-            height: item.standing_reach.height,
-          },
-        ]);
-        setCmj([
-          ...cmj,
-          {
-            date: item.date,
-            base: item.cmj.base,
-            touch: item.cmj.touch,
-            reach_height: item.cmj.reach_height,
-            jump_height: item.cmj.jump_height,
-          },
-        ]);
-        setMaximal_jump([
-          ...maximal_jump,
-          {
-            date: item.date,
-            base: item.maximal_jump.base,
-            touch: item.maximal_jump.touch,
-            reach_height: item.maximal_jump.reach_height,
-          },
-        ]);
-        setPro_agility([
-          ...pro_agility,
-          {
-            date: item.date,
-            planned_trial_1: item.pro_agility.planned_trial_1,
-            planned_trial_2: item.pro_agility.planned_trial_2,
-            reaction_trial_1: item.pro_agility.reaction_trial_1,
-            reaction_trial_2: item.pro_agility.reaction_trial_2,
-            planned_max: item.pro_agility.planned_max,
-            reaction_max: item.pro_agility.reaction_max,
-          },
-        ]);
-        setTen_yard_dash([
-          ...ten_yard_dash,
-          {
-            date: item.date,
-            trial_1: item.ten_yard_dash.trial_1,
-            trial_2: item.ten_yard_dash.trial_2,
-            result_max: item.ten_yard_dash.result_max,
-            result_average: item.ten_yard_dash.result_average,
-          },
-        ]);
-        setReaction([
-          ...reaction,
-          {
-            date: item.date,
-            trial_1_max: item.reaction.trial_1_max,
-            trial_1_average: item.reaction.trial_1_average,
-            trial_2_max: item.reaction.trial_2_max,
-            trial_2_average: item.reaction.trial_2_average,
-            result_max: item.reaction.result_max,
-            result_average: item.reaction.result_average,
-          },
-        ]);
-        setPattern_jump([
-          ...pattern_jump,
-          {
-            date: item.date,
-            sagittal_10s: item.pattern_jump.sagittal_10s,
-            frontal_10s: item.pattern_jump.frontal_10s,
-          },
-        ]);
-        setPush_up([
-          ...push_up,
-          {
-            date: item.date,
-            time: item.push_up.time,
-            repetiton: item.push_up.repetiton,
-          },
-        ]);
-        setMedicine_ball_throwing([
-          ...medicine_ball_throwing,
-          {
-            date: item.date,
-            trial_1: item.medicine_ball_throwing.trial_1,
-            trial_2: item.medicine_ball_throwing.trial_2,
-            result_max: item.medicine_ball_throwing.result_max,
-            result_average: item.medicine_ball_throwing.result_average,
-          },
-        ]);
+    
+    if (periodic.length > 0) {
+      periodic.map((item) => {
+        if (item.injury_prevention.hasOwnProperty('y_balance')) {
+          setY_balance([
+            ...y_balance,
+            {
+              date: item.date,
+              limb_length: item.injury_prevention.y_balance.limb_length,
+              anterior_left: item.injury_prevention.y_balance.anterior_left,
+              anterior_right: item.injury_prevention.y_balance.anterior_right,
+              anterior_difference: item.injury_prevention.y_balance.anterior_difference,
+              postero_lateral_left: item.injury_prevention.y_balance.postero_lateral_left,
+              postero_lateral_right: item.injury_prevention.y_balance.postero_lateral_right,
+              postero_lateral_difference:
+                item.injury_prevention.y_balance.postero_lateral_difference,
+              postero_medial_left: item.injury_prevention.y_balance.postero_medial_left,
+              postero_medial_right: item.injury_prevention.y_balance.postero_medial_right,
+              postero_medial_difference: item.injury_prevention.y_balance.postero_medial_difference,
+              composite_score_left: item.injury_prevention.y_balance.composite_score_left,
+              composite_score_right: item.injury_prevention.y_balance.composite_score_right,
+            },
+          ]);
+        }
+        if (item.injury_prevention.hasOwnProperty('tuck_jump')) {
+          setTuck_jump([
+            ...tuck_jump,
+            {
+              date: item.date,
+              score: item.injury_prevention.tuck_jump.score,
+            },
+          ]);
+        }
+        if (item.performance_test.hasOwnProperty('isometric_test')) {
+          setIsometric_test([
+            ...isometric_test,
+            {
+              date: item.date,
+              rail_point: item.performance_test.isometric_test.rail_point,
+              max_force_left: item.performance_test.isometric_test.max_force_left,
+              max_force_right: item.performance_test.isometric_test.max_force_right,
+              max_force_difference: item.performance_test.isometric_test.max_force_difference,
+              rfd_left: item.performance_test.isometric_test.rfd_left,
+              rfd_right: item.performance_test.isometric_test.rfd_right,
+              rfd_difference: item.performance_test.isometric_test.rfd_difference,
+            },
+          ]);
+        }
+        if (item.performance_test.hasOwnProperty('three_pq_both_legs')) {
+          setThree_pq_both_legs([
+            ...three_pq_both_legs,
+            {
+              date: item.date,
+              max_force: item.performance_test.three_pq_both_legs.max_force,
+              max_power: item.performance_test.three_pq_both_legs.max_power,
+              max_power_negative: item.performance_test.three_pq_both_legs.max_power_negative,
+              average_peak_power: item.performance_test.three_pq_both_legs.average_peak_power,
+              average_peak_power_negative:
+                item.performance_test.three_pq_both_legs.average_peak_power_negative,
+              decrement: item.performance_test.three_pq_both_legs.decrement,
+              decrement_negative: item.performance_test.three_pq_both_legs.decrement_negative,
+            },
+          ]);
+        }
+        if (item.performance_test.hasOwnProperty('stiffness_jump')) {
+          setStiffness_jump([
+            ...stiffness_jump,
+            {
+              date: item.date,
+              jump_height_max: item.performance_test.stiffness_jump.jump_height_max,
+              jump_height_average: item.performance_test.stiffness_jump.jump_height_average,
+              rsi_max: item.performance_test.stiffness_jump.rsi_max,
+              rsi_average: item.performance_test.stiffness_jump.rsi_average,
+            },
+          ]);
+        }
+        if (item.performance_test.hasOwnProperty('standing_reach')) {
+          setStanding_reach([
+            ...standing_reach,
+            {
+              date: item.date,
+              base: item.performance_test.standing_reach.base,
+              touch: item.performance_test.standing_reach.touch,
+              height: item.performance_test.standing_reach.height,
+            },
+          ]);
+        }
+        if (item.performance_test.hasOwnProperty('cmj')) {
+          setCmj([
+            ...cmj,
+            {
+              date: item.date,
+              base: item.performance_test.cmj.base,
+              touch: item.performance_test.cmj.touch,
+              reach_height: item.performance_test.cmj.reach_height,
+              jump_height: item.performance_test.cmj.jump_height,
+            },
+          ]);
+        }
+        if (item.performance_test.hasOwnProperty('maximal_jump')) {
+          setMaximal_jump([
+            ...maximal_jump,
+            {
+              date: item.date,
+              base: item.performance_test.maximal_jump.base,
+              touch: item.performance_test.maximal_jump.touch,
+              reach_height: item.performance_test.maximal_jump.reach_height,
+            },
+          ]);
+        }
+        if (item.performance_test.hasOwnProperty('pro_agility')) {
+          setPro_agility([
+            ...pro_agility,
+            {
+              date: item.date,
+              planned_trial_1: item.performance_test.pro_agility.planned_trial_1,
+              planned_trial_2: item.performance_test.pro_agility.planned_trial_2,
+              reaction_trial_1: item.performance_test.pro_agility.reaction_trial_1,
+              reaction_trial_2: item.performance_test.pro_agility.reaction_trial_2,
+              planned_max: item.performance_test.pro_agility.planned_max,
+              reaction_max: item.performance_test.pro_agility.reaction_max,
+            },
+          ]);
+        }
+        if (item.performance_test.hasOwnProperty('ten_yard_dash')) {
+          setTen_yard_dash([
+            ...ten_yard_dash,
+            {
+              date: item.date,
+              trial_1: item.performance_test.ten_yard_dash.trial_1,
+              trial_2: item.performance_test.ten_yard_dash.trial_2,
+              result_max: item.performance_test.ten_yard_dash.result_max,
+              result_average: item.performance_test.ten_yard_dash.result_average,
+            },
+          ]);
+        }
+        if (item.performance_test.hasOwnProperty('reaction')) {
+          setReaction([
+            ...reaction,
+            {
+              date: item.date,
+              trial_1_max: item.performance_test.reaction.trial_1_max,
+              trial_1_average: item.performance_test.reaction.trial_1_average,
+              trial_2_max: item.performance_test.reaction.trial_2_max,
+              trial_2_average: item.performance_test.reaction.trial_2_average,
+              result_max: item.performance_test.reaction.result_max,
+              result_average: item.performance_test.reaction.result_average,
+            },
+          ]);
+        }
+        if (item.performance_test.hasOwnProperty('pattern_jump')) {
+          setPattern_jump([
+            ...pattern_jump,
+            {
+              date: item.date,
+              sagittal_10s: item.performance_test.pattern_jump.sagittal_10s,
+              frontal_10s: item.performance_test.pattern_jump.frontal_10s,
+            },
+          ]);
+        }
+        if (item.performance_test.hasOwnProperty('push_up')) {
+          setPush_up([
+            ...push_up,
+            {
+              date: item.date,
+              time: item.performance_test.push_up.time,
+              repetiton: item.performance_test.push_up.repetiton,
+            },
+          ]);
+        }
+        if (item.performance_test.hasOwnProperty('medicine_ball_throwing')) {
+          setMedicine_ball_throwing([
+            ...medicine_ball_throwing,
+            {
+              date: item.date,
+              trial_1: item.performance_test.medicine_ball_throwing.trial_1,
+              trial_2: item.performance_test.medicine_ball_throwing.trial_2,
+              result_max: item.performance_test.medicine_ball_throwing.result_max,
+              result_average: item.performance_test.medicine_ball_throwing.result_average,
+            },
+          ]);
+        }
       });
     }
-  }, []);
+  }, [periodic]);
 
   return (
     <div className="pb-10">
